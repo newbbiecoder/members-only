d9ef373c670beed06839e33a0e316c1e10df9a90
Rudraksh Kumar <rudrakshkumar2908@gmail.com>
2026-02-22 00:58:56 +0530

    Changed connectstring to our neon database

--- config/pool.js
+++ config/pool.js
@@ -2,11 +2,10 @@ const {Pool} = require("pg");
 require("dotenv").config({path: ".env"});
 
 const pool = new Pool({
-    host: "localhost",
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    password: process.env.DATABASE_PASS,
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 })
 
 module.exports = pool;
